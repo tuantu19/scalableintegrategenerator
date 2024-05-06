02e4ee011ad7a5777466055b640e9d5a45bf49c0
tuantu19 <piobrossjfpun0suy@hotmail.com>
2024-05-07 03:56:17 +0700

    update sdk

---
+++ index.js
@@ -0,0 +1,7 @@
+const groupBy = (arr, func) =>
+  arr
+    .map(typeof func === "function" ? func : (val) => val[func])
+    .reduce((acc, val, i) => {
+      acc[val] = (acc[val] || []).concat(arr[i]);
+      return acc;
+    }, {});
