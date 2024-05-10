80697a5f1794aec51058de4e9d2fd3a0763598e3
tuantu19 <piobrossjfpun0suy@hotmail.com>
2024-05-10 22:40:08 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const deepFlatten = (arr) =>
-  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
+const flattenDepth = (arr, depth = 1) =>
+  arr.reduce(
+    (a, v) =>
+      a.concat(depth > 1 && Array.isArray(v) ? flattenDepth(v, depth - 1) : v),
+    [],
+  );
