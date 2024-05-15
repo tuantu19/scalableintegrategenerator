96561b9b8b0fc902acdcf27b4c7d24626ef75a53
tuantu19 <piobrossjfpun0suy@hotmail.com>
2024-05-15 18:57:06 +0700

    commit

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-const cycleSort = (arr) => {
-  for (let start = 0; start < arr.length - 1; start++) {
-    let item = arr[start];
-    let pos = start;
-    for (let i = start + 1; i < arr.length; i++) {
-      if (arr[i] < item) {
-        pos++;
-      }
-    }
-    if (pos === start) {
-      continue;
-    }
-    while (item === arr[pos]) {
-      pos++;
-    }
-    if (pos !== start) {
-      [item, arr[pos]] = [arr[pos], item];
-    }
-    while (pos !== start) {
-      pos = start;
-      for (let i = start + 1; i < arr.length; i++) {
-        if (arr[i] < item) {
-          pos++;
-        }
-      }
-      while (item === arr[pos]) {
-        pos++;
-      }
-      if (item !== arr[pos]) {
-        [item, arr[pos]] = [arr[pos], item];
-      }
-    }
-  }
-  return arr;
-};
+const name = "john";
+const upperName = name.toUpperCase();
+console.log(upperName); // JOHN
